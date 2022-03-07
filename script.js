d9893dcfd99abcfd8df1406358423f702dad089b
Halden-Aratro <marianne.bousquet1@gmail.com>
2022-03-07 20:24:37 +0100

    attempted to fix faulty playSelection

--- script.js
+++ script.js
@@ -3,9 +3,14 @@ let playerScore = 0;
 let roundWinner = '';
 
 //player chooses paper, rock or scissors
-let choice = prompt('Please, make your choice by writing "rock", "paper" or "scissors"');
-const playerSelection = choice;
-     
+let playerSelection = prompt('Please, make your choice by writing "rock", "paper" or "scissors"');
+
+//function playerChooses() {
+    //choice = prompt('rock paper scissors');
+    //playerSelection = choice;
+    //return playSelection;
+//}
+
 //computer randomly selects 1 of the 3 choices
 const computerSelection = computerPlay();
 function computerPlay() {
@@ -24,24 +29,31 @@ function computerPlay() {
 
 //a round is played (need to just play and not display any result to screen)
 function playRound (playerSelection, computerSelection) {
-    playerSelection = choice.toLowerCase();
-    computerSelection = computerPlay().toLowerCase();
+    playerSelection = playerSelection.toLowerCase(); console.log(playerSelection);
+    computerSelection = computerPlay().toLowerCase(); console.log(computerSelection);
     if (playerSelection === computerSelection) {
         roundWinner = ("Tie")
     } else if (
         (playerSelection === "rock" && computerSelection === "paper") ||
         (playerSelection === "paper" && computerSelection === "scissors") ||
         (playerSelection === "scissors" && computerSelection === "rock")
-    ) {
+    )   {
         computerScore++;
         roundWinner = 'computer';
         return roundWinner;
-    } else {
+    } else if (
+        (playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "paper" && computerSelection === "rock") ||
+        (playerSelection === "scissors" && computerSelection === "paper")
+    )   {
         playerScore++;
         roundWinner = 'player';
         return roundWinner;
     }; 
 }
+
+console.log(playerScore);
+console.log(computerScore);
 
 //create a game of 5 rounds
 function game () {
