a68e1844b2fa4a0b6b5423de62b042f82ee5795f
Halden-Aratro <marianne.bousquet1@gmail.com>
2022-03-07 11:31:05 +0100

    corrected indentation

--- script.js
+++ script.js
@@ -1,44 +1,46 @@
-            //game begins
-            //round 1 players choose their moves
-            function playRound (playerSelection, computerSelection) {
-                playerSelection = choice.toLowerCase();
-                computerSelection = computerPlay().toLowerCase();
-                if (playerSelection === computerSelection) {
-                    alert("Tie")
-                } else if (
-                    (playerSelection === "rock" && computerSelection === "paper") ||
-                    (playerSelection === "paper" && computerSelection === "scissors") ||
-                    (playerSelection === "scissors" && computerSelection === "rock")
-                ) {
-                    alert("the computer wins")
-                } else {
-                    alert ("you win")
-                } 
-            }
+//game begins
+//round 1 players choose their moves
 
-                //human chooses paper, rock or scissors
-            let choice = prompt('Please, make your choice by writing "rock", "paper" or "scissors"');
-            const playerSelection = choice;
+
+//human chooses paper, rock or scissors
+let choice = prompt('Please, make your choice by writing "rock", "paper" or "scissors"');
+const playerSelection = choice;
             
                 //computer randomly returns paper, rock or scissors
-            const computerSelection = computerPlay();
+const computerSelection = computerPlay();
 
-            function computerPlay() {
-                let randomNum = Math.floor(Math.random() * 3);
-                switch(randomNum) {
-                    case 0:
-                        return "rock";
-                        break;
-                    case 1:
-                        return "paper";
-                        break;
-                    case 2: 
-                        return "scissors";
-                }
-            }
+function computerPlay() {
+    let randomNum = Math.floor(Math.random() * 3);
+    switch(randomNum) {
+    case 0:
+        return "rock";
+        break;
+    case 1:
+        return "paper";
+        break;
+    case 2: 
+        return "scissors";
+    }
+}
 
-            console.log(playRound(playerSelection, computerSelection));
+console.log(playRound(playerSelection, computerSelection));
 
-                //the winner is declared
+function playRound (playerSelection, computerSelection) {
+    playerSelection = choice.toLowerCase();
+    computerSelection = computerPlay().toLowerCase();
+    if (playerSelection === computerSelection) {
+        alert("Tie")
+    } else if (
+        (playerSelection === "rock" && computerSelection === "paper") ||
+        (playerSelection === "paper" && computerSelection === "scissors") ||
+        (playerSelection === "scissors" && computerSelection === "rock")
+    ) {
+        alert("the computer wins")
+    } else {
+        alert ("you win")
+    }; 
+    console.log(playerSelection);
+    console.log(computerSelection);
+}
 
 
