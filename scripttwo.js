046afd852e6ba93a93394362533c5ec311aac0d6
Halden-Aratro <marianne.bousquet1@gmail.com>
2022-03-12 17:17:56 +0100

    completed scores msg in game fx

--- scripttwo.js
+++ scripttwo.js
@@ -1,6 +1,5 @@
 let computerScore = 0;
 let playerScore = 0;
-let result = '';
 
 let computerSelection;
 
@@ -21,28 +20,43 @@ function computerPlay() {
 
 function playerPlay() {
     let playerSelection = prompt("chose rock, paper or scissors");
+    playerSelection = playerSelection.toLowerCase();
+
+    if ((playerSelection =! "rock") || 
+        (playerSelection =! "paper") || 
+        (playerSelection =! "scissors")) {
+            prompt("only enter rock, paper or scissors without spelling mistakes")
+        }
+
     return playerSelection;
 }
 
 function playRound(playerSelection, computerSelection) {
+    const SHOW_CHOICES = `(Player chose: ${playerSelection}  and Computer chose ${computerSelection})`
     const TIE_MSG = "Tie";
     const WIN_MSG = "You win";
     const LOSE_MSG = "You lose";
 
     if ((playerSelection === "rock" && computerSelection === "rock") ||
-           (playerSelection === "paper" && computerSelection === "paper") ||
-           (playerSelection === "scissors" && computerSelection === "scissors")) {
+        (playerSelection === "paper" && computerSelection === "paper") ||
+        (playerSelection === "scissors" && computerSelection === "scissors")) {
         console.log(WIN_MSG);
+        console.log(SHOW_CHOICES);
         playerScore += 1;
+        showScores();
     } else if ((playerSelection === "rock" && computerSelection === "paper") ||
                (playerSelection === "paper" && computerSelection === "scissors") ||
                (playerSelection === "scissors" && computerSelection === "rock")) {
         console.log(LOSE_MSG);
+        console.log(SHOW_CHOICES);
         computerScore += 1;
+        showScores();
     } else if ((playerSelection === "rock" && computerSelection === "scissors") ||
                (playerSelection === "paper" && computerSelection === "rock") ||
                (playerSelection === "scissors" && computerSelection === "paper")) {
         console.log(TIE_MSG);
+        console.log(SHOW_CHOICES);
+        showScores();
     }
 }
 
@@ -63,22 +77,27 @@ function game() {
     } 
 
     console.log("Final score: ");
+    declareWinner();
 
       
 }
 
-
+function showScores() {
+    const SCORES = `player score: ${playerScore} | computer score: ${computerScore}`;
+    
+    console.log(SCORES);
+}
 
 
 function declareWinner() {
     const SCORES = `player score: ${playerScore} | computer score: ${computerScore}`;
 
     if (playerScore > computerScore) {
-        console.log("You won!" + SCORES);
+        console.log("You won the game!\n" + SCORES);
     } else if (playerScore < computerScore) {
-        console.log("You lost!" + SCORES);
+        console.log("You lost the game! Really?!\n" + SCORES);
     } else {
-        console.log("This is not possible" + SCORES);
+        console.log("This is a final tie " + SCORES);
     }
 
 }
